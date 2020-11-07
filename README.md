dd4c61f34fd139fa4ab246301172cc5b64d36a8b
unknown <lv_dongy@163.com>
2020-11-07 15:51:49 +0800

    完成

---
+++ README.md
@@ -0,0 +1,82 @@
+# collapse Attributes
+
+参数 | 说明 | 类型 | 可选值 | 默认值
+|  ----  | ----  | ----  | ----  | ----  |
+v-model | 绑定值，控制内容展开收起 | Boolean | - | -
+default-height | 默认显示内容的高度（单位px） | Number | - | 100
+hide-controll | 是否隐藏内置控制按钮 | Boolean | - | false
+hide-controll-icon | 是否隐藏内置控制按钮的icon（在hide-controll为false的情况下该属性才生效） | Boolean | - | false
+fold-text| 内置控制按钮收起的文案 | String | - | “收起”
+unfold-text| 内置控制按钮展开的文案 | String | - | “展开”
+
+<br>
+
+# collapse Slots
+
+name | 说明
+----  | ----  |
+- | 组件中的内容
+
+<br>
+
+# Methods
+
+方法名 | 说明 |  参数  |
+----  | ----  | ----  |
+fold | 收起内容 | -
+unfold | 展开内容 | -
+refresh | 手动刷新组件状态（组件状态自动更新异常时可尝试着手动调用刷新） | -
+
+<br>
+
+# 基础用法
+
+直接把展示内容放置到组件中，并设置一个默认高度即可
+
+```HTML
+<collapse :default-height="70">
+    <div>这是内容...</div>
+    <div>很多很多内容...</div>
+    ...
+</collapse>
+```
+
+<br>
+
+# 使用v-model
+
+用一个变量绑定组件的状态，并可通过改变来变量控制组件的状态
+```HTML
+<collapse v-model="isOpen" :default-height="70">
+    <div>这是内容...</div>
+    <div>很多很多内容...</div>
+</collapse>
+```
+```javascript
+data(){
+    return {
+        isOpen: true,
+    }
+}
+```
+
+<br>
+
+# 自定义开关按钮
+
+隐藏内置开关按钮，利用v-model控制或者手动调用方法控制
+
+```HTML
+<collapse v-model="isOpen" :default-height="70" hide-controll>
+    <div>这是内容...</div>
+    <div>很多很多内容...</div>
+</collapse>
+<span class="btn" @click="isOpen = !isOpen">{{isOpen ? '关' : '开'}}</span>
+```
+```javascript
+data(){
+    return {
+        isOpen: false,
+    }
+}
+```
---
+++ index.vue
@@ -0,0 +1,176 @@
+<template>
+    <section 
+        class="collapse" 
+        ref="collapse" 
+        :style="{height: containerH + 'px', 'padding-right': (hideControll ? 0 : 50) + 'px'}"
+    >
+        <div class="collapse-content" ref="content">
+            <slot></slot>
+        </div>
+        <span v-if="!hideControll" v-show="controllVisible" class="collapse-controll" @click="handleControll">
+            <i v-show="!hideControllIcon" class="collapse-controll-icon" :class="isShow ? 'el-icon-arrow-up' : 'el-icon-arrow-down'"></i>
+            {{isShow ? foldText : unfoldText}}
+        </span>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'collapse',
+
+    model: {
+        prop: 'visible',
+        event: 'change'
+    },
+
+    props: {
+        visible: Boolean,
+
+        // 默认显示的内容高度
+        defaultHeight: {
+            type: Number,
+            default: 100
+        },
+
+        // 是否隐藏内置控制按钮
+        hideControll: Boolean,
+
+        // 是否隐藏内置控制按钮icon
+        hideControllIcon: Boolean,
+
+        foldText: {
+            type: String,
+            default: '收起'
+        },
+
+        unfoldText: {
+            type: String,
+            default: '展开'
+        }
+    },
+
+    data(){
+        return {
+            isShow: false,
+            controllVisible: true,
+
+            containerH: this.defaultHeight,
+            contentH: 0,
+            DOMWatcher: null,
+        }
+    },
+
+    watch: {
+        visible(v){
+            if(v){
+                this.unfold();
+            }else{
+                this.fold();
+            }
+        },
+
+        isShow(v){
+            this.$emit('change', v);
+        }
+    },
+
+    mounted(){
+        this.init();
+        this.watch();
+
+        if(this.visible){
+            this.unfold();
+        }
+    },
+
+    destroyed(){
+        this.DOMWatcher.disconnect();
+    },
+
+    methods: {
+        init(){
+            this.contentH = this.$refs.content.offsetHeight;
+            /**
+             * 当内容变化的同时也是展开状态的话，那么需要更新容器高度
+             * 1. 当最新的内容高度大于默认高度时，则把容器高度更新最新内容高度一致即可
+             * 2. 反之，把容器高度设到最小（即默认高度），同时设为收起状态
+             */
+            if(this.isShow){
+                if(this.contentH > this.defaultHeight){
+                    this.containerH = this.contentH;
+                }else{
+                    this.containerH = this.defaultHeight;
+                    this.isShow = false;
+                }
+            }
+            this.controllVisible = this.contentH > this.defaultHeight; // 控制按钮的显示隐藏
+        },
+
+        watch(){
+            this.DOMWatcher = new MutationObserver(() =>{
+                // 监测到DOM变化，重新计算高度
+                this.refresh();
+            })
+            this.DOMWatcher.observe(this.$refs.content, {
+                childList: true,
+                attributes: true,
+                characterData: true,
+                subtree: true
+            })
+        },
+
+        refresh(){
+            this.$nextTick(() => {
+                this.init();
+            })
+        },
+
+        handleControll(){
+            let handle = this.isShow ? this.fold : this.unfold;
+            handle();
+        },
+
+        // 收起
+        fold(){
+            this.containerH = this.defaultHeight;
+            this.$nextTick(() => {
+                this.isShow = false;
+            })
+        },
+
+        // 展开
+        unfold(){
+            const contentH = this.$refs.content.offsetHeight;
+            if(contentH > this.defaultHeight){
+                this.containerH = contentH;
+            }
+            this.$nextTick(() => {
+                this.isShow = true;
+            })
+        },
+    }
+}
+</script>
+
+<style lang='scss' scope>
+.collapse{
+    width: 100%;
+    position: relative;
+    overflow: hidden;
+    transition: height .28s ease-in-out;
+
+    &-controll{
+        position: absolute;
+        bottom: 0px;
+        right: 8px;
+        font-size: 14px;
+        font-weight: 400;
+        color: #6ABCFF;
+        cursor: pointer;
+
+        &-icon{
+            margin-right: 2px;
+        }
+    }
+}
+</style>
